738558ebd50e8bbe48958d8fd33e5d0cc28579d3
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-10-01 16:28:22 +0800

    新增 Express Request 介面的 user 屬性類型定義

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,12 @@
+import type { User as UserModel } from "../models/Association.js"
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserModel;
+        }
+    }
+}
+
+// 防止 TypeScript 將這個檔案視為一個全域的 script
+export {};
