26963a3f1cf120f205c73898c7279e01c81c56d6
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-10-28 17:45:34 +0800

    去掉不用的 getHistory

--- client/src/components/Context/authContext.tsx
+++ client/src/components/Context/authContext.tsx
@@ -1,21 +1,16 @@
 import {
   createContext,
   useContext,
-  useEffect,
   useState,
   useCallback,
   useMemo,
 } from "react";
 import { saveToken, clearToken } from "../../service/auth.service";
-import { getHistory } from "../../service/analyze.service";
-import type { HistoryItem } from "../../types/Analyze";
 
 interface AuthContextValue {
   isAuthenticated: boolean;
   login: (token: string) => Promise<void>;
   logout: () => Promise<void>;
-  history: HistoryItem[];
-  setToggleAnalysis: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 interface AuthProviderProps {
@@ -26,41 +21,15 @@ const AuthContext = createContext<AuthContextValue | null>(null);
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [history, setHistory] = useState<HistoryItem[]>([]);
-  const [toggleAnalysis, setToggleAnalysis] = useState(false)
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const token = localStorage.getItem("token");
-      setIsAuthenticated(!!token);
-
-      if (!token) return;
-
-      try {
-        const data = await getHistory();
-        setHistory(data.history);
-      } catch (error) {
-        console.log("load user data failed", error);
-      }
-    };
-    fetchUser();
-  }, [toggleAnalysis]);
 
   const login = async (token: string) => {
     saveToken(token);
     setIsAuthenticated(true);
-    try {
-      const data = await getHistory();
-      setHistory(data.history);
-    } catch (error) {
-      console.error("fetch history failed", error);
-    }
   };
 
   const logout = useCallback(async () => {
     clearToken();
     setIsAuthenticated(false);
-    setHistory([]);
   }, []);
 
   const value = useMemo<AuthContextValue>(
@@ -68,10 +37,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
       isAuthenticated,
       login,
       logout,
-      history,
-      setToggleAnalysis,
     }),
-    [isAuthenticated, login, logout, history, setToggleAnalysis]
+    [isAuthenticated, login, logout]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
